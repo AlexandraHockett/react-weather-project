a3eb7a26c5bab03fb7a4745969c3646a4c55d4b2
AlexandraHockett <87495152+AlexandraHockett@users.noreply.github.com>
2021-11-27 15:14:30 +0000

    Added the API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,73 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Lisbon",
-    date: "Updated on Sat/Oct 30, 14:28",
-    temperature: "20",
-    feelsLike: "20º",
-    imgUrl: "http://openweathermap.org/img/wn/09d@2x.png",
-    description: "Light intensity drizzle rain",
-    máx: "21º",
-    min: "18º",
-    humidity: "92%",
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      date: "Updated on Sat/Oct 30, 14:28",
+      temperature: Math.round(response.data.main.temp),
+      feelsLike: Math.round(response.data.main.feels_like),
+      imgUrl: "http://openweathermap.org/img/wn/09d@2x.png",
+      description: response.data.weather[0].description,
+      máx: Math.round(response.data.main.temp_max),
+      min: Math.round(response.data.main.temp_min),
+      humidity: response.data.main.humidity,
+      city: response.data.name,
+    });
+  }
 
-  return (
-    <div className="Weather">
-      <div className="citySearch">
-        <form>
-          <input
-            type="search"
-            placeholder="Type a city..."
-            autoComplete="off"
-          />
-          <input type="submit" value="Search" className="search" />
-          <button>Current Location</button>
-        </form>
-        <br />
-      </div>
-      <div className="overview">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>{weatherData.date}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-md-6">
-          <h2 id="temperature">{weatherData.temperature}</h2>
-          <span className="units">
-            <a href="/">ºC</a>|<a href="/">ºF</a>
-          </span>
-          <span className="feels">feels like</span>
-          <div className="feelsDegree" id="feelsLike">
-            {weatherData.feelsLike}
-          </div>
-          <div className="picweather">
-            <img src={weatherData.imgUrl} alt="Weather pic" id="icon" />
-          </div>
-          <span className="picDescription" id="description">
-            {weatherData.description}
-          </span>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="citySearch">
+          <form>
+            <input
+              type="search"
+              placeholder="Type a city..."
+              autoComplete="off"
+            />
+            <input type="submit" value="Search" className="search" />
+            <button>Current Location</button>
+          </form>
+          <br />
         </div>
-        <div className="col-md-6">
-          <ul className="details">
-            <li>
-              <strong>Máx: </strong>
-              {weatherData.máx}
-            </li>
-            <li>
-              <strong>Min: </strong>
-              {weatherData.min}
-            </li>
-            <li>
-              <strong>Humidity:</strong>
-              {weatherData.humidity}
-            </li>
+        <div className="overview">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
           </ul>
         </div>
+        <div className="row">
+          <div className="col-md-6">
+            <h2 id="temperature">{weatherData.temperature}</h2>
+            <span className="units">
+              <a href="/">ºC</a>|<a href="/">ºF</a>
+            </span>
+            <span className="feels">feels like</span>
+            <div className="feelsDegree" id="feelsLike">
+              {weatherData.feelsLike}º
+            </div>
+            <div className="picweather">
+              <img src={weatherData.imgUrl} alt="Weather pic" id="icon" />
+            </div>
+            <span className="picDescription" id="description">
+              {weatherData.description}
+            </span>
+          </div>
+          <div className="col-md-6">
+            <ul className="details">
+              <li>
+                <strong>Máx: </strong>
+                {weatherData.máx}º
+              </li>
+              <li>
+                <strong>Min: </strong>
+                {weatherData.min}º
+              </li>
+              <li>
+                <strong>Humidity:</strong>
+                {weatherData.humidity}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "06fbd7d55cead2045835eef5076a763f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
