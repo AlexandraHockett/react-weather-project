451fd5b67a602496e9653b9256344cf52c7fb442
AlexandraHockett <87495152+AlexandraHockett@users.noreply.github.com>
2021-11-27 15:00:21 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/alexandra-hockett/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Alexandra Hockett{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/AlexandraHockett/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/alexandra-hockett/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alexandra Hockett{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/AlexandraHockett/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
